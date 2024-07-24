5646e6589ccbd725dbaf8041b147e60aa0eaca18
Mohammed Rahil Hussain <mohammedrahilhussain@Mohammeds-MacBook-Pro.local>
2024-07-24 14:34:06 -0400

    added base springboot framework

---
+++ prem-stats/src/main/java/com/pl/prem_stats/PremStatsApplication.java
@@ -0,0 +1,13 @@
+package com.pl.prem_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremStatsApplication.class, args);
+	}
+
+}
---
+++ prem-stats/src/test/java/com/pl/prem_stats/PremStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.prem_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
